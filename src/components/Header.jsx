bbfd13735a3bbc575a6c3ccf50fed3432b5abe50
ManuelPeva <manuel_859@outlook.es>
2024-03-25 21:21:29 -0600

    terminacion de inicio de sesion

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,40 @@
 /* eslint-disable no-unused-vars */
 // eslint-disable-next-line no-unused-vars
 import {FaSignInAlt, FaSignOutAlt, FaUser} from 'react-icons/fa'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
+import { useSelector, useDispatch } from 'react-redux'
+import {logout, reset} from '../features/auth/authSlice'
+
+
 
 const Headers = () => {
+
+    const dispatch = useDispatch();
+    const navigate = useNavigate()
+
+    const {user} = useSelector((state)=> state.auth)
+
+    const onLogout = () => {
+        dispatch(logout())
+        dispatch(reset())
+        dispatch('/')
+    }
+
   return (
     <header className='header'>
         <div className="logo">
             <Link to='/'>MoviPeli</Link>
         </div>
         <ul>
-            <li>
+            {user ? (
+                <li>
+                    <button className='btn' onClick={onLogout}> 
+                        <FaSignOutAlt/> Logout
+                        </button>
+                </li>
+            ):(
+                <>
+                <li>
                 <Link to='/login'>
                     <FaSignInAlt/>Login
                 </Link>
@@ -22,6 +46,9 @@ const Headers = () => {
                     <FaUser/>Registrar
                 </Link>
             </li>
+                </>
+            )}
+            
         </ul>
     </header>
   )
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,4 +1,21 @@
+import { useEffect } from "react"
+import {useNavigate} from 'react-router-dom'
+import { useDispatch, useSelector } from "react-redux"
+
+
 const Dashboard = () => {
+
+  const navigate = useNavigate()
+  const dispatch = useDispatch()
+
+  const {user} = useSelector((state)=> state.auth)
+
+  useEffect(()=>{
+    if(!user){
+      navigate('/login')
+    }
+  },[user, navigate])
+
   return (
     <div>
       <h1>Dashboard</h1>
